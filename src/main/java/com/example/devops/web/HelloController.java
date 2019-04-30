2fb1d045af6ff0d320544ff21cbca640b7b21513
yiren <yi.ren@ibm.com>
2019-04-30 14:39:24 -0300

    controller

--- src/main/java/com/example/devops/web/HelloController.java
+++ src/main/java/com/example/devops/web/HelloController.java
@@ -5,13 +5,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
+@RequestMapping("/")
 public class HelloController {
 
 	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
-	@RequestMapping("/", method = GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public String welcome(Model model) {
 		logger.info("Processing index request");
 		model.addAttribute("course", "DevOps");
